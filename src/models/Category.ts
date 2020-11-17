ff97cc756988dc0b73971c99bb5493b9f267964b
vinicius cesar <Viniciuscesarlemes@gmail.com>
2020-11-17 19:39:26 -0400

    17/11/2020

---
+++ src/models/Category.ts
@@ -0,0 +1,29 @@
+import { Transition } from 'react-native-reanimated';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  OneToMany,
+} from 'typeorm';
+
+import Transaction from './Transaction';
+
+@Entity('categories')
+class Category {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  title: string;
+
+  @OneToMany(() => Transaction, transaction => transaction.category)
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default Category;
